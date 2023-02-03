22361e417cd56602af46d1582448d184e5dc6d1c
irajmoradi <irajjmoradi@gmail.com>
2023-02-03 10:02:19 -0800

    Forgot to activate actions on github before last commit

--- Fixme.py
+++ Fixme.py
@@ -3,6 +3,6 @@
 
 def evens(n):
     '''
-    Returns a list of even numbers from 0 to n inclusive.
+    Returns a list of even numbers from 0 to n inclusive
     '''
     return list(filter(lambda x: x % 2 == 0, range(n + 1)))
